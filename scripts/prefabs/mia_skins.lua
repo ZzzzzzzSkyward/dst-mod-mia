2b2b6c9c1842a858acd58b22d41239cc95db4297
zzzzzzzskyward <zzzzzzzskyward@github.io>
2023-06-05 23:09:47 +0800

    fix ghost build, fix cookpot

--- scripts/prefabs/mia_skins.lua
+++ scripts/prefabs/mia_skins.lua
@@ -1,27 +1,36 @@
 local assets = {Asset("ANIM", "anim/%s.zip"), Asset("ANIM", "anim/ghost_%s_build.zip")}
 
-local skins = {normal_skin = "%s", ghost_skin = "ghost_%s_build"}
+local skins = {
+  normal_skin = "%s",
+  ghost_skin = "ghost_%s_build"
+}
 -- #NOTPLANNED this is simplified because there is actually no skins
 local params = {
-    base_prefab = "%s",
-    type = "base",
-    assets = assets,
-    skins = skins,
-    skin_tags = {"%s", "CHARACTER", "BASE"},
-    build_name = "%s",
-    rarity = "Character",
-    build_name_override="%s"
+  base_prefab = "%s",
+  type = "base",
+  assets = assets,
+  skins = skins,
+  skin_tags = {"%s", "CHARACTER", "BASE"},
+  build_name = "%s",
+  rarity = "Character",
+  build_name_override = "%s"
 }
 local prefs = {}
 local chars = require("mia_characters")
 local name_pattern = "%s_none"
-local function fmt(tb, name)
-    local t = shallowcopy(tb)
-    for i, v in pairs(t) do t[i] = type(v) == "string" and v:format(name) or (type(v)=="table" and fmt(v,name) or v) end
-    return t
+local function fmt(tb, name, override)
+  local t = shallowcopy(tb)
+  for i, v in pairs(t) do
+    if override and override[i] then
+      t[i] = override[i]
+    else
+      t[i] = type(v) == "string" and v:format(name) or (type(v) == "table" and fmt(v, name) or v)
+    end
+  end
+  return t
 end
 for name, v in pairs(chars) do
-    local this_param = fmt(params, name)
-    table.insert(prefs, CreatePrefabSkin(name_pattern:format(name), this_param))
+  local this_param = fmt(params, name, v)
+  table.insert(prefs, CreatePrefabSkin(name_pattern:format(name), this_param))
 end
 return unpack(prefs)
