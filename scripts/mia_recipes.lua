577fb6ff0b84c70bd5f96873cfea45b9db366a6e
zzzzzzzskyward <zzzzzzzskyward@github.io>
2023-06-05 16:44:52 +0800

    disable scaled umbrella recipe

--- scripts/mia_recipes.lua
+++ scripts/mia_recipes.lua
@@ -11,8 +11,8 @@ AddRecipe2("riko_sack", {Ingredient("bearger_fur", 1), Ingredient("papyrus", 4),
     {builder_tag = "riko"}, {"MODS", "CLOTHING", "RIKO"})
 AddRecipe2("nanachitent", {Ingredient("goose_feather", 5), Ingredient("cutgrass", 12), Ingredient("petals", 5)},
     TECH.SCIENCE_TWO, {placer = "nanachitent_placer", builder_tag = "nanachi"}, {"MODS", "STRUCTURES", "NANACHI"})
-AddRecipe2("scaled_umbrella", {Ingredient("umbrella", 1), Ingredient("charcoal_sand", 60), Ingredient("rope", 8)},
-    TECH.NONE, {builder_tag = "riko"}, {"MODS", "WEAPON", "TOOL", "RIKO"})
+--AddRecipe2("scaled_umbrella", {Ingredient("umbrella", 1), Ingredient("charcoal_sand", 60), Ingredient("rope", 8)},
+--    TECH.NONE, {builder_tag = "riko"}, {"MODS", "WEAPON", "TOOL", "RIKO"})
 -- Gives back a mitty later
 -- Health value is set to character's max health*60% later
 AddRecipe2("mitty_tea_bundle", {Ingredient("mitty_bottled", 1), Ingredient(CHARACTER_INGREDIENT.MAX_HEALTH, 0)},
