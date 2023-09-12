5e5e43e4da9cda3d4dae7d93412d1257a424b8cb
zzzzzzzs <55184223+ZzzzzzzSkyward@users.noreply.github.com>
2023-09-12 11:09:24 +0800

    sync physics util with gallop mod

--- scripts/mia_common_physics.lua
+++ scripts/mia_common_physics.lua
@@ -1,19 +1,41 @@
 local function MakeCharacterNoPassThrough(inst)
-    local phys = inst.Physics
-    phys:ClearCollisionMask()
-    phys:CollidesWith(COLLISION.WORLD)
-    phys:CollidesWith(COLLISION.OBSTACLES)
-    phys:CollidesWith(COLLISION.SMALLOBSTACLES)
-    phys:CollidesWith(COLLISION.CHARACTERS)
-    phys:CollidesWith(COLLISION.GIANTS)
+  local phys = inst.Physics
+  phys:ClearCollisionMask()
+  phys:CollidesWith(COLLISION.WORLD)
+  phys:CollidesWith(COLLISION.OBSTACLES)
+  phys:CollidesWith(COLLISION.SMALLOBSTACLES)
+  phys:CollidesWith(COLLISION.CHARACTERS)
+  phys:CollidesWith(COLLISION.GIANTS)
 end
 local function MakeCharacterPassThrough(inst)
-    local phys = inst.Physics
-    phys:ClearCollisionMask()
-    phys:CollidesWith(COLLISION.GROUND)
-    phys:CollidesWith(COLLISION.OBSTACLES)
-    phys:CollidesWith(COLLISION.SMALLOBSTACLES)
-    phys:CollidesWith(COLLISION.CHARACTERS)
-    phys:CollidesWith(COLLISION.GIANTS)
+  local phys = inst.Physics
+  phys:ClearCollisionMask()
+  phys:CollidesWith(COLLISION.GROUND)
+  phys:CollidesWith(COLLISION.OBSTACLES)
+  phys:CollidesWith(COLLISION.SMALLOBSTACLES)
+  phys:CollidesWith(COLLISION.CHARACTERS)
+  phys:CollidesWith(COLLISION.GIANTS)
 end
-return {MakeCharacterPassThrough = MakeCharacterPassThrough, MakeCharacterNoPassThrough = MakeCharacterNoPassThrough}
+local function ToggleOffPhysicsNoPassThrough(inst)
+  inst.sg.statemem.isphysicstoggle = true
+  inst.Physics:ClearCollisionMask()
+  inst.Physics:CollidesWith(COLLISION.WORLD)
+end
+local function ToggleOffPhysics(inst)
+  inst.sg.statemem.isphysicstoggle = true
+  inst.Physics:ClearCollisionMask()
+  inst.Physics:CollidesWith(COLLISION.GROUND)
+end
+
+local function ToggleOnPhysics(inst)
+  inst.sg.statemem.isphysicstoggle = nil
+  MakeCharacterNoPassThrough(inst)
+end
+
+return {
+  ToggleOnPhysics = ToggleOnPhysics,
+  ToggleOffPhysicsNoPassThrough= ToggleOffPhysicsNoPassThrough,
+  ToggleOffPhysics = ToggleOffPhysics,
+  MakeCharacterPassThrough = MakeCharacterPassThrough,
+  MakeCharacterNoPassThrough = MakeCharacterNoPassThrough
+}
